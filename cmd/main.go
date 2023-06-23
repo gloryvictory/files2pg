9cd23a958205a79cacc83b9762db331c1c0c58e4
Vyacheslav Zamaraev <zamaraev@gmail.com>
2023-06-23 15:40:30 +0500

    add config file

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"files2pg/internal/config"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "test"
+	password = "testpwd"
+	dbname   = "test"
+)
+
+func main() {
+	config, err := config.LoadConfig()
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(config)
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
